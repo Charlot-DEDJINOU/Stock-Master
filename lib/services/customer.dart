ac6cc11030c61d4c64263e375a64273339510676
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-24 18:16:12 +0100

    Customer Services

---
+++ lib/services/customer.dart
@@ -0,0 +1,74 @@
+import 'package:stock_master/models/customer.dart';
+import 'package:stock_master/api.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CustomerServive {
+  Dio api = Api.api();
+
+  Future<Customer> create(Map<String, dynamic> data) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.post('customer', data: data);
+
+    return Customer.fromJson(response.data);
+  }
+
+  Future<Customer> get(String id) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.get('customer/$id');
+
+    return Customer.fromJson(response.data);
+  }
+
+  Future<List<Customer>> getAll() async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.get('customers');
+
+    return (response.data['data'] as List)
+        .map((e) => Customer.fromJson(e))
+        .toList();
+  }
+
+  Future<Customer> update(String id, Map<String, dynamic> data) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.put('customer/$id', data: data);
+
+    return Customer.fromJson(response.data);
+  }
+
+  Future<void> delete(String id) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    await api.delete('customer/$id');
+  }
+  
+}
