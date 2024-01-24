f0a621b0a7edcadea29ebf20b9574a1c619c18ef
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-24 18:10:29 +0100

    Model Customer

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+List<Customer> customerFromJson(String str) => List<Customer>.from(json.decode(str).map((x) => Customer.fromJson(x)));
+
+String customerToJson(List<Customer> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class Customer {
   int customerId;
   String customerName;
