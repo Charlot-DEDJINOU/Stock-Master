a1f0c9471a8d064cfcc93ae8c64b3c03c9846ff5
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-24 18:16:45 +0100

    Product Services

---
+++ lib/services/product.dart
@@ -0,0 +1,74 @@
+import 'package:stock_master/models/product.dart';
+import 'package:stock_master/api.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProductServive {
+  Dio api = Api.api();
+
+  Future<Product> create(Map<String, dynamic> data) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.post('product', data: data);
+
+    return Product.fromJson(response.data);
+  }
+
+  Future<Product> get(String id) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.get('product/$id');
+
+    return Product.fromJson(response.data);
+  }
+
+  Future<List<Product>> getAll() async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.get('products');
+
+    return (response.data['data'] as List)
+        .map((e) => Product.fromJson(e))
+        .toList();
+  }
+
+  Future<Product> update(String id, Map<String, dynamic> data) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    final response = await api.put('product/$id', data: data);
+
+    return Product.fromJson(response.data);
+  }
+
+  Future<void> delete(String id) async {
+    final pref = await SharedPreferences.getInstance();
+    String token = pref.getString("token") ?? "";
+
+    if (token != "") {
+      api.options.headers['AUTHORIZATION'] = 'Bearer $token';
+    }
+
+    await api.delete('product/$id');
+  }
+  
+}
