61ff72b3fdf4871eb12d80a0a20cf08c0d1f4430
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-24 18:10:46 +0100

    Model Product

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+List<Product> productFromJson(String str) => List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+
+String productToJson(List<Product> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class Product {
   int productId;
   String productName;
