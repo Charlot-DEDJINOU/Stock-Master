c3710776bbb7b251a0e80ecb83030cd0c568c6bf
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-25 23:50:34 +0100

    modidy login url

--- lib/services/user.dart
+++ lib/services/user.dart
@@ -3,19 +3,17 @@ import 'package:stock_master/api.dart';
 import 'package:dio/dio.dart';
 
 class UserService {
-
   Dio api = Api.api();
 
-  Future<AuthenticatedUser> login (Map<String, dynamic> data) async{
-    final response = await api.post('authentication', data: data);
+  Future<AuthenticatedUser> login(Map<String, dynamic> data) async {
+    final response = await api.post('login', data: data);
 
     return AuthenticatedUser.fromJson(response.data);
   }
 
-  Future<User> create (Map<String, dynamic> data) async{
-    final response = await api.post('users', data: data);
+  Future<User> create(Map<String, dynamic> data) async {
+    final response = await api.post('register', data: data);
 
     return User.fromJson(response.data);
   }
-
 }
