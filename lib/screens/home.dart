7a69d5a546145842bba32267472385bb859076b9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-27 10:56:09 +0100

    add options

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:stock_master/screens/layout/drawer.dart';
-import 'package:stock_master/screens/layout/bottom_navigation_bar.dart';
+import 'package:stock_master/screens/category/caterogies.dart';
+import 'package:stock_master/layout/appbar.dart';
+import 'package:stock_master/layout/drawer.dart';
+import 'package:stock_master/layout/bottom_navigation_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,24 +15,62 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Stock Master" , style: TextStyle(color: Colors.white)), 
-        backgroundColor: const Color(0xFF02BB02),
-          leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer(); // Ouvrez le menu latéral
-              },
-            );
-          },
-        ),
+      appBar: const CustomAppBar(title: 'Stock Master'),
+      drawer: const CustomAppDrawer(),
+      bottomNavigationBar: const CustomBottomNavigationBar(index: 0),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          _buildCard(Icons.inventory, 'Produits', true, () {
+            // Action à effectuer lors du clic sur le premier rectangle
+          }),
+          _buildCard(Icons.category, 'Categories', false,  () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const ShowCaterories(),
+              ));
+          }),
+          _buildCard(Icons.person, 'Clients', false, () {
+            // Action à effectuer lors du clic sur le troisième rectangle
+          }),
+           _buildCard(Icons.shopping_cart, 'Commandes', true, () {
+            // Action à effectuer lors du clic sur le deuxième rectangle
+          }),
+          _buildCard(Icons.business_center, 'Fournisseurs', true, () {
+            // Action à effectuer lors du clic sur le quatrième rectangle
+          }),
+          _buildCard(Icons.payment, 'Achats', false, () {
+            // Action à effectuer lors du clic sur le quatrième rectangle
+          }),
+          _buildCard(Icons.attach_money, 'Ventes', false, () {
+            // Action à effectuer lors du clic sur le troisième rectangle
+          }),
+          _buildCard(Icons.timeline, 'Previsions', true, () {
+            // Action à effectuer lors du clic sur le quatrième rectangle
+          }),
+        ],
       ),
-      drawer: const AppDrawer(),
-      bottomNavigationBar: const BottomNavigationBarExample(),
-      body: const Center(
-        child: Text('Contenu de la page d\'accueil'),
+    );
+  }
+
+  Widget _buildCard(IconData icon, String text, bool position, Function() onPressed) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Card(
+        elevation: 8,
+        color: position ? Colors.green : Colors.brown,
+        margin: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 48, color: Colors.white),
+            const SizedBox(height: 16),
+            Text(
+              text,
+              style: const TextStyle(fontSize: 18, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
