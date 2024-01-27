8c7341c6903fc6fb71104bf4082b01846b33fb43
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-27 10:57:20 +0100

    CustomAppBar

---
+++ lib/layout/appbar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:stock_master/screens/about.dart';
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({super.key, required this.title});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        widget.title,
+        style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: const Color(0xFF02BB02),
+      leading: Builder(
+        builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(Icons.menu, color: Colors.white),
+            onPressed: () {
+              Scaffold.of(context).openDrawer(); // Ouvrez le menu latéral
+            },
+          );
+        },
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const About(),
+            ));
+          },
+          icon: const Icon(Icons.info, color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
