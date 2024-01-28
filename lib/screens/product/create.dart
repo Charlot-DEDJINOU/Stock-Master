e47ccfd7251c6a260f36fc20b48f817185197d09
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-28 22:50:39 +0100

    product

---
+++ lib/screens/product/create.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateProduct extends StatefulWidget {
+  const CreateProduct({super.key});
+
+  @override
+  State<CreateProduct> createState() => _CreateProductState();
+}
+
+class _CreateProductState extends State<CreateProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/product/products.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowProduts extends StatefulWidget {
+  const ShowProduts({super.key});
+
+  @override
+  State<ShowProduts> createState() => _ShowProdutsState();
+}
+
+class _ShowProdutsState extends State<ShowProduts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
