84e424fdcffed0ad50fd136e7170bca9e5567514
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-29 23:59:14 +0100

    handleUnauthorizedError

---
+++ lib/layout/handle_unauthorized_error.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stock_master/layout/toast.dart';
+import 'package:stock_master/screens/login.dart';
+
+void handleUnauthorizedError(BuildContext context) async {
+  final pref = await SharedPreferences.getInstance();
+  pref.setString("token", "");
+  
+  showToast("Veuillez vous authentifier");
+
+  Navigator.of(context).pushReplacement(
+    MaterialPageRoute(builder: (context) => const Login()),
+  );
+}
