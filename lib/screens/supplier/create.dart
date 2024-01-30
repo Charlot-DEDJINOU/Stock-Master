2aaac2bfd7052741345d877889af49868569c322
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-30 20:12:53 +0100

    Page to create Supplier

---
+++ lib/screens/supplier/create.dart
@@ -0,0 +1,173 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:stock_master/layout/appbar.dart';
+import 'package:stock_master/layout/bottom_navigation_bar.dart';
+import 'package:stock_master/layout/drawer.dart';
+import 'package:stock_master/layout/handle_unauthorized_error.dart';
+import 'package:stock_master/layout/toast.dart';
+import 'package:stock_master/screens/supplier/suppliers.dart';
+import 'package:stock_master/services/supplier.dart';
+
+class CreateSupplier extends StatefulWidget {
+  const CreateSupplier({Key? key}) : super(key: key);
+
+  @override
+  State<CreateSupplier> createState() => _CreateSupplierState();
+}
+
+class _CreateSupplierState extends State<CreateSupplier> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+
+  SupplierServive supplierServive = SupplierServive();
+
+  create(data) async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var response = await supplierServive.create(data);
+      print(response);
+      showToast("Fournisseurs enregistré avec succès");
+      nameController.text = "";
+      emailController.text = "";
+      phoneController.text = "";
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const ShowSuppliers()));
+    } on DioException catch (e) {
+      print(e);
+
+      if (e.response!.statusCode == 401) {
+        handleUnauthorizedError(context);
+      } else if (e.response != null) {
+        showToast("Une erreur est intervenue");
+        print(e.response!.data);
+      } else {
+        showToast("Une erreur est intervenue");
+        print(e.message);
+      }
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Nouveau Fournisseur"),
+      drawer: const CustomAppDrawer(),
+      bottomNavigationBar: const CustomBottomNavigationBar(index: 0),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: 150,
+                    height: 150,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email du Fournisseur',
+                          prefixIcon: Icon(Icons.email),
+                        ),
+                        validator: (String? value) {
+                          return value == null || value.isEmpty
+                              ? 'Ce champ est obligatoire'
+                              : null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Nom du Fournisseur',
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                        validator: (String? value) {
+                          return value == null || value.isEmpty
+                              ? 'Ce champ est obligatoire'
+                              : null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        keyboardType: TextInputType.phone,
+                        controller: phoneController,
+                        decoration: const InputDecoration(
+                          labelText: 'Numéro du Fournisseur',
+                          prefixIcon: Icon(Icons.phone),
+                          hintText: '22959105267',
+                        ),
+                        validator: (String? value) {
+                          return value == null || value.isEmpty
+                              ? 'Ce champ est obligatoire'
+                              : null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      await create({
+                        "supplier_name": nameController.text,
+                        "email": emailController.text,
+                        "phone": phoneController.text,
+                      });
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF02BB02),
+                    padding: EdgeInsets.zero,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  child: isLoading
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : const Text(
+                          'Ajouter',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
